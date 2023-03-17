bf41f1904f5169d36c18f2435e7b53c7cf006931
nnamdi ezeh <45914831+shagariboy@users.noreply.github.com>
2023-03-17 20:45:50 +0100

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+
+
+export default function Example() {
+    
+  return (
+    <><div className="bg-white dark:bg-black">
+          <div className="flex flex-wrap items-center lg:h-5/6 lg:my-24 ">
+              <div
+                  className="w-80 h-60 mx-auto my-6 shadow-lg rounded-md transform transition hover:shadow-xl hover:-translate-y-0.5 dark:bg-gray-800">
+                  <div className="flex flex-wrap">
+                      <div className="w-60">
+                          <img className="my-4 h-24 rounded-full mx-auto cursor-pointer transition duration-300 transform hover:scale-110"
+                              src="/assets/avatar.8df22d7f.png" alt="avatar" />
+                          <p className="mt-2 mb-0 text-xl text-center font-serif font-extrabold dark:text-gray-200">BeaCox</p>
+                          <p className="-mt-1 text-l text-center font-sans font-light text-gray-400"> On the way to be a hacker
+                          </p>
+                          <div className="my-4 w-full text-center">
+                              <a className="inline-block mx-2 px-4 py-2 rounded-lg transform transition bg-blue-500 hover:bg-blue-400 hover:-translate-y-0.5 focus:ring-blue-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-1 active:bg-blue-600 uppercase tracking-wider font-semibold text-xs text-white shadow-lg"
+                                  href="https://blog.beacox.space" target="_blank" rel="noreferrer">
+                                  Blog
+                              </a>
+                              <a className="inline-block mx-2 px-4 py-2 rounded-lg transform transition bg-blue-500 hover:bg-blue-400 hover:-translate-y-0.5 focus:ring-blue-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-1 active:bg-blue-600 uppercase tracking-wider font-semibold text-xs text-white shadow-lg"
+                                  href="https://blog.beacox.space/atom.xml" target="_blank" rel="noreferrer">
+                                  RSS
+                              </a>
+                          </div>
+                      </div>
+                      <div className="w-20">
+                          <a className=" h-1/3 py-5 w-full rounded-tr-md inline-block box-border border-b-2 border-white bg-yellow-200 text-gray-500 text-center transform transition hover:text-blue-400 hover:bg-yellow-100"
+                              href="https://github.com/BeaCox" target="_blank" rel="noreferrer">
+                              <p className="social"><i className="iconfont icon-github"></i></p>
+                          </a>
+                          <a className=" h-1/3 py-5 w-full block box-border border-b-2 border-white bg-yellow-200 text-gray-500 text-center transform transition hover:text-blue-400 hover:bg-yellow-100"
+                              href="https://t.me/BeaCox" target="_blank" rel="noreferrer">
+                              <p className="social"><i className="iconfont icon-telegram"></i></p>
+                          </a>
+                          <a className=" h-1/3 py-5 w-full rounded-br-md block box-border bg-yellow-200 text-gray-500 text-center transform transition hover:text-blue-400 hover:bg-yellow-100"
+                              href="mailto:root@beacox.space" target="_blank" rel="noreferrer">
+                              <p className="social"><i className="iconfont icon-email"></i></p>
+                          </a>
+                      </div>
+                  </div>
+              </div>
+              <div
+                  className="w-11/12 h-auto mx-auto mt-6 shadow-lg rounded-lg  transform transition hover:shadow-xl hover:-translate-y-0.5 lg:w-6/12 dark:bg-gray-800">
+                  <div className="mx-6 my-4 font-sans text-center dark:text-gray-400">
+                  <h3 className="tracking-[0.5rem] text-[#03dac6] even:transition-all duration-[0.3s] ease-[ease] z-[3] m-0 pb-2"><span className="m">G</span><span className="m">O</span><span className="m">I</span><span className="m">N</span><span className="m">G</span><span className="m">&nbsp;</span><span className="m">T</span><span className="m">O</span><span className="m">&nbsp;</span><span className="m">M</span><span className="m">A</span><span className="m">R</span><span className="m">K</span><span className="m">E</span><span className="m">T</span><span className="m">&nbsp;</span><span className="m">M</span><span className="m">A</span><span className="m">D</span><span className="m">E</span><span className="m">&nbsp;</span><span className="m">E</span><span className="m">A</span><span className="m">S</span><span className="m">Y</span></h3>
+                      
+                  </div>
+              </div>
+          </div>
+      </div><div className="flex">
+              <div className="mx-auto my-4 lg:-my-4">
+                  <p className="text-center text-gray-400">Copyright © 2023 <a className="font-bold hover:underline hover:text-gray-600" href="https://nd-porfolio.netlify.app/" target="_blank" rel="noreferrer">Shagariboy</a></p>
+                  <p className="text-center text-gray-400"><a className="hover:underline hover:text-gray-600 transform transition" href="https://www.linkedin.com/in/nnamdi-ezeh-2658031ba/" target="_blank" rel="noreferrer">Linkedin</a></p>
+              </div>
+          </div></>
+  );
+}
